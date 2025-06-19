61f35878c154f07189eb211c125026fe39275c25
WenaRastaMan <seb.juarez@duocuc.cl>
2025-06-19 22:53:19 +0200

    Add files via upload

---
+++ microservicio-de-gestion-de-clases/src/main/java/com/example/microservicio/de/gestion/de/clases/controller/ServicioController.java
@@ -0,0 +1,5 @@
+package com.example.microservicio.de.gestion.de.clases.controller;
+
+public class ServicioController {
+
+}
---
+++ microservicio-de-gestion-de-clases/src/main/java/com/example/microservicio/de/gestion/de/clases/repository/ClaseRepository.java
@@ -0,0 +1,5 @@
+package com.example.microservicio.de.gestion.de.clases.repository;
+
+public interface ClaseRepository {
+
+}
