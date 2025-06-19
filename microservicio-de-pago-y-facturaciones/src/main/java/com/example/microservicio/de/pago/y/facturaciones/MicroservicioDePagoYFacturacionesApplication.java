1140df97bb3ceb876fd03c499c28dc07a9543471
WenaRastaMan <seb.juarez@duocuc.cl>
2025-06-19 22:54:52 +0200

    Add files via upload

---
+++ microservicio-de-pago-y-facturaciones/src/main/java/com/example/microservicio/de/pago/y/facturaciones/MicroservicioDePagoYFacturacionesApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservicio.de.pago.y.facturaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioDePagoYFacturacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioDePagoYFacturacionesApplication.class, args);
+	}
+
+}
---
+++ microservicio-de-pago-y-facturaciones/src/test/java/com/example/microservicio/de/pago/y/facturaciones/MicroservicioDePagoYFacturacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservicio.de.pago.y.facturaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioDePagoYFacturacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
