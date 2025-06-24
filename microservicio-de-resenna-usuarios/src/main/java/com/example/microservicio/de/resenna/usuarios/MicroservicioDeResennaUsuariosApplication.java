83d20716f673746f4c55f64112ad18526b2db0ab
WenaRastaMan <seb.juarez@duocuc.cl>
2025-06-24 11:15:11 -0400

    Add files via upload

---
+++ microservicio-de-resenna-usuarios/src/main/java/com/example/microservicio/de/resenna/usuarios/MicroservicioDeResennaUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservicio.de.resenna.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioDeResennaUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioDeResennaUsuariosApplication.class, args);
+	}
+
+}
